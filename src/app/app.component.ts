71c7bf8296106ffc81e6dc2937b61debec800266
Fernando <Fernando@Fernando-PC>
2017-06-25 18:01:21 -0300

    Alterando os atributos className do JSX por class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core';
     selector: 'meu-app',
     template: `
       <div>
-        <nav className="indigo darken-4" role="navigation">
-            <div className="nav-wrapper container">
-              <a id="logo-container" href="#" className="brand-logo">Angular Taskboard</a>
+        <nav class="indigo darken-4" role="navigation">
+            <div class="nav-wrapper container">
+              <a id="logo-container" href="#" class="brand-logo">Angular Taskboard</a>
             </div>
         </nav>
 
@@ -14,17 +14,17 @@ import { Component } from '@angular/core';
           <lista-estoria></lista-estoria>
         </div>
 
-        <footer className="page-footer orange">
-          <div className="container">
-            <div className="row">
-              <div className="col l6 s12">
+        <footer class="page-footer orange">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
                 Introdução ao Angular
               </div>
             </div>
           </div>
-          <div className="footer-copyright">
-            <div className="container">
-            Feito com <a className="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+          <div class="footer-copyright">
+            <div class="container">
+            Feito com <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
             </div>
           </div>
         </footer>
